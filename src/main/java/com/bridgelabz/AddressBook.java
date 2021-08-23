db0116f3e23e42a0020739322b9b362dcd8e99b9
bhavani125 <girinenibhavani12@gmil.com>
2021-08-23 21:16:28 +0530

    Added:code of updating contact information

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,28 +1,41 @@
 package com.bridgelabz;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class AddressBook {
-    public enum IOService {
-            DB_IO
-        }
 
-        private List<AddressBookData> addressBookList;
-        public final AddressBookDBService addressBookDBService;
+    public enum IOService {
+        DB_IO
+    }
+
+    private List<AddressBookData> addressBookList;
+    public final AddressBookDBService addressBookDBService;
 
     public AddressBook() {
-            addressBookDBService = AddressBookDBService.getInstance();
-        }
+        addressBookDBService = AddressBookDBService.getInstance();
+    }
 
     public AddressBook(List<AddressBookData> addressBookList) {
-            this();
-            this.addressBookList = addressBookList;
-        }
+        this();
+        this.addressBookList = addressBookList;
+    }
 
-        public List<AddressBookData> readAddressBookData(IOService ioService) {
-            if (ioService.equals(IOService.DB_IO))
-                return this.addressBookList = addressBookDBService.getAddressBookDataUsingDB();
-            return null;
-        }
+    public List<AddressBookData> readAddressBookData(IOService ioService) throws SQLException {
+        if (ioService.equals(IOService.DB_IO))
+            return this.addressBookList = addressBookDBService.getAddressBookDataUsingDB();
+        return null;
+    }
+    public void updateContact(String address, String name) {
+        int result = addressBookDBService.updateContactDetails(name, address);
+    }
+
+    public boolean checkAddressBookInSyncWithDB(String name) {
+        List<AddressBookData> addressBookDataList = addressBookDBService.getEmployeePayrollData(name);
+        return addressBookDataList.get(0).equals(getEmployeePayrollData(name));
+    }
+
+    private AddressBookData getEmployeePayrollData(String name) {
+        return this.addressBookList.stream().filter(employeePayrollDataItem -> employeePayrollDataItem.firstName.equals(name)).findFirst().orElse(null);
+    }
 }
-
--- src/test/java/com/bridgelabz/AddressBookTest.java
+++ src/test/java/com/bridgelabz/AddressBookTest.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class AddressBookTest {
@@ -10,9 +11,17 @@ public class AddressBookTest {
     List<AddressBookData> addressBookDataList;
 
     @Test
-    public void givenThreeContactsInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+    public void givenThreeContactsInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws SQLException {
         addressBook = new AddressBook();
         //Assertions.assertEquals(3, addressBookDataList.size());
         Assertions.assertEquals(4,  addressBook.readAddressBookData(AddressBook.IOService.DB_IO).size());
+    }
+    @Test
+    public void givenContactDataInDB_whenUpdated_ShouldSyncWithDB() throws SQLException {
+        addressBook = new AddressBook();
+        addressBookDataList = addressBook.readAddressBookData(AddressBook.IOService.DB_IO);
+        addressBook.updateContact("ShivamRoad", "Bhavani");
+        boolean result = addressBook.checkAddressBookInSyncWithDB("Bhavani");
+        Assertions.assertTrue(result);
     }
 }
